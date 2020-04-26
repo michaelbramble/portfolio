15dc46521cb873ac9b38c5509d84f29143d8210c
Michael Bramble <mike.w.bramble@gmail.com>
2020-04-26 05:45:00 -0400

    updated smw site page, changed the code that displays

--- src/Components/Projects/SmwSite.js
+++ src/Components/Projects/SmwSite.js
@@ -20,7 +20,7 @@ export default class SmwSite extends Component {
             completely from scratch using React, but is currently still a large
             work in progress.
           </p>
-          <p>Last updated: November 24, 2019</p>
+          <p>Last updated: April 26, 2020</p>
           <h2>Tech Stack</h2>
           <ul className="techStack">
             <li>Client: HTML, CSS, JavaScript, React, React Router.</li>
@@ -35,58 +35,62 @@ export default class SmwSite extends Component {
           </div>
         </main>
         <section className='codeContainer' id='code'>
-          <p id='ind0'>{`import React, {Component} from 'react';`}</p>
-          <p id='ind0'>{`import {Link} from 'react-router-dom';`}</p>
-          <p id='ind0'>{`import LEVELS from '../../Data/levelslist';`}</p>
-          <p id='ind0'>{`import './levelpage.css';`}</p>
-          <br />
-          <p id='ind0'>{'export default class LevelPage extends Component {'}</p>
-          <p id='ind1'>{'toggleVideo(id){'}</p>
-          <p id='ind2'>{'let frame = document.getElementById(id);'}</p>
-          <p id='ind2'>{`if(frame.getAttribute('src') === ''){`}</p>
-          <p id='ind3'>{`alert('There is no video for this strat yet.');`}</p>
-          <p id='ind2'>{'}'}</p>
-          <p id='ind2'>{'else{'}</p>
-          <p id='ind3'>{`if(frame.style.display === 'block'){`}</p>
-          <p id='ind4'>{`frame.style.display = 'none';`}</p>
-          <p id='ind3'>{'}'}</p>
-          <p id='ind3'>{'else {'}</p>
-          <p id='ind4'>{`frame.style.display = 'block';`}</p>
-          <p id='ind3'>{'}'}</p>
-          <p id='ind2'>{'}'}</p>
-          <p id='ind1'>{'}'}</p>
-          <br />
-          <p id='ind1'>{'render(){'}</p>
-          <p id='ind2'>{'const level = LEVELS.filter(l => l.id === this.props.match.params.id);'}</p>
-          <br />
-          <p id='ind2'>{'return ('}</p>
-          <p id='ind3'>{`<section className='levpage-container'>`}</p>
-          <p id='ind4'>{`<h1 id='level-name'>{level[0].name}</h1>`}</p>
-          <p id='ind4'>{'<small>'}</p>
-          <p id='ind5'>{`{'Previous & Next Levels in 96 Exit Route: '}`}</p>
-          <p id='ind5'>{'<Link to={`/levels/${level[0].prev.id}`}>{level[0].prev.name}</Link>'}</p>
-          <p id='ind5'>{'{' / '}'}</p>
-          <p id='ind5'>{'<Link to={`/levels/${level[0].next.id}`}>{level[0].next.name}</Link>'}</p>
-          <p id='ind4'>{'</small>'}</p>
-          <p id='ind4'>{'{level[0].strats.map(s =>'}</p>
-          <p id='ind5'>{`<div className='category'>`}</p>
-          <p id='ind6'>{'<h3 id={s.catId}>{s.category}</h3>'}</p>
-          <p id='ind6'>{'{s.videos.map(v =>'}</p>
-          <p id='ind7'>{`<div className='strat'>`}</p>
-          <p id='ind8'>{`<h4 id='vid-title'>{v.title}</h4>`}</p>
-          <p id='ind8'>{`<button className='button' onClick={() => this.toggleVideo(v.id)}>Show/Hide Video</button>`}</p>
-          <p id='ind8'>{`<iframe src={v.link} title={v.title} id={v.id} className='stratVid' frameBorder='0' allowFullScreen={true} />`}</p>
-          <p id='ind8'>{`<p id='info'><b>Optimal Time: {v.opTime}</b>`}</p>
-          <p id='ind8'>{'<br />'}</p>
-          <p id='ind8'>{'{v.info}</p>'}</p>
-          <p id='ind7'>{'</div>'}</p>
-          <p id='ind6'>{')}'}</p>
-          <p id='ind5'>{'</div>'}</p>
-          <p id='ind4'>{')}'}</p>
-          <p id='ind3'>{'</section>'}</p>
-          <p id='ind2'>{')'}</p>
-          <p id='ind1'>{'}'}</p>
-          <p id='ind0'>{'}'}</p>
+          <p id='ind0'>{`render(){`}</p>
+          <p id='ind1'>{`return (`}</p>
+          <p id='ind2'>{`<section className='level-page-container'>`}</p>
+          <p id='ind3'>{`<h1>{this.state.l.name}</h1>`}</p>
+          <p id='ind3'>{`<ul className='level-categories'>`}</p>
+          <p id='ind4'>{`{this.state.l.categories.map(c =>`}</p>
+          <p id='ind5'>{`<li key={c.id} onClick={(e) => this.setState({activeCategory: c})}>{c.title}</li>`}</p>
+          <p id='ind4'>{`)}`}</p>
+          <p id='ind3'>{`</ul>`}</p>
+          <p id='ind3'>{`<div className='level-active-category'>`}</p>
+          <p id='ind4'>{`<h2>{this.state.activeCategory.title}</h2>`}</p>
+          <p id='ind4'>{`<h3>{this.state.activeCategory.exits.normal ? 'Normal Exit' : ''}</h3>`}</p>
+          <p id='ind4'>{`<ul className='active-strats-normal'>`}</p>
+          <p id='ind5'>{`{this.state.activeCategory.exits.normal ? this.state.activeCategory.exits.normal.strats.map(s => {`}</p>
+          <p id='ind6'>{`return (`}</p>
+          <p id='ind6'>{`<li key={s.id} onClick={(e) => this.setState({activeStrat: s})}>`}</p>
+          <p id='ind7'>{"{s.name}{s.time ? ` - ${s.time}` : ''}"}</p>
+          <p id='ind7'>{`<br />`}</p>
+          <p id='ind7'>{`{s.id === this.state.activeStrat.id ? (`}</p>
+          <p id='ind8'>{`<ul id={s.id}>`}</p>
+          <p id='ind9'>{`<li><a href={s.link}>Link</a></li>`}</p>
+          <p id='ind9'>{`{s.desc === '' ? '' : <li>{s.desc}</li>}`}</p>
+          <p id='ind9'>{`<li>Source: {s.source}</li>`}</p>
+          <p id='ind8'>{`</ul>`}</p>
+          <p id='ind7'>{`) : ''}`}</p>
+          <p id='ind6'>{`</li>`}</p>
+          <p id='ind6'>{`)`}</p>
+          <p id='ind5'>{`}) : ''}`}</p>
+          <p id='ind4'>{`</ul>`}</p>
+          <p id='ind4'>{`<h3>{this.state.activeCategory.exits.secret ? 'Secret Exit' : ''}</h3>`}</p>
+          <p id='ind4'>{`<ul className='active-strats-secret'>`}</p>
+          <p id='ind5'>{`{this.state.activeCategory.exits.secret ? this.state.activeCategory.exits.secret.strats.map(s => {`}</p>
+          <p id='ind6'>{`return (`}</p>
+          <p id='ind6'>{`<li key={s.id} onClick={(e) => this.setState({activeStrat: s})}>`}</p>
+          <p id='ind7'>{"{s.name}{s.time ? ` - ${s.time}` : ''}"}</p>
+          <p id='ind7'>{`<br />`}</p>
+          <p id='ind7'>{`{s.id === this.state.activeStrat.id ? (`}</p>
+          <p id='ind8'>{`<ul id={s.id}>`}</p>
+          <p id='ind9'>{`<li><a href={s.link}>Link</a></li>`}</p>
+          <p id='ind9'>{`{s.desc === '' ? '' : <li>{s.desc}</li>}`}</p>
+          <p id='ind9'>{`<li>Source: {s.source}</li>`}</p>
+          <p id='ind8'>{`</ul>`}</p>
+          <p id='ind7'>{`) : ''}`}</p>
+          <p id='ind6'>{`</li>`}</p>
+          <p id='ind6'>{`)`}</p>
+          <p id='ind5'>{`}) : ''}`}</p>
+          <p id='ind4'>{`</ul>`}</p>
+          <p id='ind3'>{`</div>`}</p>
+          <p id='ind3'>{`<div className='level-info'>`}</p>
+          <p id='ind4'>{`<img src={this.state.l.image} alt='Screenshot' />`}</p>
+          <p id='ind4'>{`<p>Available Powerups: {this.state.l.powerups.join(', ')}</p>`}</p>
+          <p id='ind4'>{`<p>This level {this.state.l.dragoncoins ? 'has' : 'does not have'} Dragon Coins.</p>`}</p>
+          <p id='ind3'>{`</div>`}</p>
+          <p id='ind2'>{`</section>`}</p>
+          <p id='ind1'>{`)`}</p>
+          <p id='ind0'>{`}`}</p>
         </section>
       </div>
     );
